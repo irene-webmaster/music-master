6cad15d664f90204c59600efce1f13e7cb2662f4
Irina Shakhova <carina.omsk@gmail.com>
2017-03-15 15:54:38 -0700

    Added Bootstrap and styled the Main Page

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import './app.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <div>
-          <div className="app-title">Music Master</div>
-        </div>
-        <div>
-          <input placeholder="Search an artist" />
-          <button>button</button>
-        </div>
+      <div className="app">
+
+        <div className="app-title">Music Master</div>
+
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Search for an Artist"
+            />
+            <InputGroup.Addon>
+              <Glyphicon glyph="search"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+
         <div className="profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
         </div>
+
         <div className="gallery">
           Gallery
         </div>
+
       </div>
     )
   }
